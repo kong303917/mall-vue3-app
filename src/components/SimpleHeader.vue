eeff62b5fe89a578edab500977ec1c02b239a1b8
zhoukongcheng <zhoukongcheng@zhoukongchengdeMacBook-Air.local>
2020-11-09 10:09:12 +0800

    init

---
+++ src/components/SimpleHeader.vue
@@ -0,0 +1,59 @@
+<template>
+  <header class="simple-header">
+    <i class="nbicon nbfanhui" @click="goBack"></i>
+    <div class="simple-header-name">{{ name }}</div>
+    <i class="nbicon nbmore"></i>
+  </header>
+</template>
+
+<script>
+import { useRouter } from 'vue-router'
+export default {
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    back: {
+      type: String,
+      default: ''
+    }
+  },
+  emits: ['callback'],
+  setup(props, ctx) {
+    const router = useRouter()
+    const goBack = () => {
+      if (!props.back) {
+        router.go(-1)
+      } else {
+        router.push({ path: props.back })
+      }
+      ctx.emit('callback')
+    }
+    return {
+      goBack
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  @import '../common/style/mixin';
+  .simple-header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 10000;
+    .fj();
+    .wh(100%, 44px);
+    line-height: 44px;
+    padding: 0 10px;
+    .boxSizing();
+    color: #252525;
+    background: #fff;
+    border-bottom: 1px solid #dcdcdc;
+    .simple-header-name {
+      font-size: 14px;
+    }
+  }
+</style>
